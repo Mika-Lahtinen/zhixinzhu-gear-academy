93c30f6cf2aa3cfe9b8cef50d9ec14d5206fd777
Kurtis Moxley <image1224@outlook.com>
2024-08-01 04:06:16 +0800

    Redo init project.

---
+++ pebble-game/src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+use gstd::{};
+use pebble_game_io::*;
+
+static mut PEBBLE_GAME:Option<GameState>=None;
+
+#[no_mangle]
+extern "C" fn init() {
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+}
+
+#[no_mangle]
+extern "C" fn state() {
+}
