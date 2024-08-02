69a0b2c1f155778660782befbd4ee7d827ec67d3
Kurtis Moxley <image1224@outlook.com>
2024-08-02 23:12:51 +0800

    Finished handle function.

--- pebble-game/src/lib.rs
+++ pebble-game/src/lib.rs
@@ -2,7 +2,7 @@
 
 use core::panic;
 
-use auxilar::get_init_pebbles_start;
+use auxilar::get_init_pebbles_remaining;
 use gstd::*;
 use pebble_game_io::*;
 
@@ -14,13 +14,9 @@ static mut PEBBLE_GAME: Option<GameState> = None;
 extern "C" fn init() {
     let config: PebblesInit = msg::load().expect("Init Error.");
 
-    if config.max_pebbles_per_turn > config.pebbles_count {
-        panic!("Max pebbles per turn cannot be greater than pebbles count.");
-    }
-
     let first_player = auxilar::check_first_player();
 
-    let init_game = get_init_pebbles_start(
+    let init_game = auxilar::get_init_pebbles_remaining(
         config.difficulty,
         config.pebbles_count,
         config.max_pebbles_per_turn,
@@ -40,7 +36,72 @@ extern "C" fn init() {
 
 #[no_mangle]
 extern "C" fn handle() {
-    unimplemented!();
+    let action: PebblesAction = msg::load().expect("Get PebbleAction Error.");
+
+    let mut game_state = unsafe { PEBBLE_GAME.clone().expect("Get state failed.") };
+    match action {
+        PebblesAction::Turn(pebbles_got) => {
+            if pebbles_got > game_state.max_pebbles_per_turn || pebbles_got == 0 {
+                panic!("Invalid input, please try again.");
+            }
+
+            if pebbles_got > game_state.pebbles_remaining {
+                panic!("Remainig is not enough, please try again.");
+            }
+
+            game_state.pebbles_remaining -= pebbles_got;
+
+            if game_state.pebbles_remaining == 0 {
+                game_state.winner = Some(Player::User);
+                msg::reply(PebblesEvent::Won(Player::User), 0)
+                    .expect("Failed to turn to Won.");
+            } else {
+                let counter_pebbles = auxilar::get_contract_pebbles(
+                    game_state.difficulty,
+                    game_state.pebbles_count,
+                    game_state.max_pebbles_per_turn,
+                );
+                game_state.pebbles_remaining -= counter_pebbles;
+                if game_state.pebbles_remaining == 0 {
+                    game_state.winner = Some(Player::Program);
+                    msg::reply(PebblesEvent::Won(Player::Program), 0)
+                        .expect("Failed to turn to Won.");
+                } else {
+                    msg::reply(PebblesEvent::CounterTurn(counter_pebbles), 0).expect("Failed to turn to CounterTurn.");
+                }
+            }
+        }
+        PebblesAction::GiveUp => {
+            game_state.winner = Some(Player::Program);
+            msg::reply(PebblesEvent::Won(Player::Program), 0)
+                .expect("Failed to turn to Won.");
+        }
+        PebblesAction::Restart {
+            difficulty,
+            pebbles_count,
+            max_pebbles_per_turn,
+        } => {
+            let first_player = auxilar::check_first_player();
+            let pebbles_remaining = auxilar::get_init_pebbles_remaining(
+                difficulty,
+                pebbles_count,
+                max_pebbles_per_turn,
+                first_player.clone(),
+            );
+            game_state = GameState {
+                pebbles_count,
+                max_pebbles_per_turn,
+                pebbles_remaining,
+                difficulty,
+                first_player,
+                winner: None,
+            };
+        }
+    }
+
+    unsafe {
+        PEBBLE_GAME = Some(game_state);
+    }
 }
 
 #[no_mangle]
